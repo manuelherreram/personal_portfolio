25f02f4f1b3c34b9e977236d849585495383183b
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-04-29 13:08:26 -0400

    Adds footer and menu effects

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,36 @@
+import "./Footer.css";
+import footer_logo from "../../assets/footer_logo.svg";
+import user_icon from "../../assets/user_icon.svg";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <img src={footer_logo} alt="" />
+          <p>I am a Full-stack developer from Valparaiso, Chile with.....</p>
+        </div>
+        <div className="footer-top-right">
+          <div className="footer-email-input">
+            <img src={user_icon} alt="" />
+            <input type="email" placeholder="Enter your email" />
+          </div>
+          <div className="div footer-subscribe">Subscribe</div>
+        </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">
+          © 2024 Manuel Herrera. All rights reserved.
+        </p>
+        <div className="footer-bottom-right">
+          <p>Term of Services</p>
+          <p>Privacy Policy</p>
+          <p>Connect with me</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
